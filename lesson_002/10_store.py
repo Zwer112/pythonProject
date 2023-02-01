dedcbdcf19592351d35e54396798e3d98ce38612
Админ <sm_s@tut.by>
2023-02-01 12:39:12 +0300

    resh

--- lesson_002/10_store.py
+++ lesson_002/10_store.py
@@ -35,6 +35,8 @@ store = {
 # например для ламп
 
 lamps_cost = store[goods['Лампа']][0]['quantity'] * store[goods['Лампа']][0]['price']
+
+
 # или проще (/сложнее ?)
 lamp_code = goods['Лампа']
 lamps_item = store[lamp_code][0]
@@ -42,6 +44,30 @@ lamps_quantity = lamps_item['quantity']
 lamps_price = lamps_item['price']
 lamps_cost = lamps_quantity * lamps_price
 print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
+
+stol_code = goods['Стол']
+stol_item0 = store[stol_code][0]
+stol_item1 = store[stol_code][1]
+stol_quantity0 = stol_item0['quantity']
+stol_quantity1 = stol_item1['quantity']
+stol_price0 = stol_item0['price']
+stol_price1 = stol_item1['price']
+stol_cost = stol_price1*stol_quantity0 + stol_price1*stol_quantity1
+print('Стол -', stol_quantity1+stol_quantity0, 'шт, стоимость', stol_cost, 'руб')
+
+stul_code = goods['Стул']
+stul_item0 = store[stul_code][0]
+stul_item1 = store[stul_code][1]
+stul_item2 = store[stul_code][2]
+stul_qw0 = stul_item0['quantity']
+stul_qw1 = stul_item1['quantity']
+stul_qw2 = stul_item2['quantity']
+stul_pr0 = stul_item0['price']
+stul_pr1 = stul_item1['price']
+stul_pr2 = stul_item2['price']
+
+stul_cost = stul_pr1*stul_qw1 + stul_qw0*stul_pr1 + stul_pr2*stul_qw2
+print('Стул -', stul_qw1+stul_qw0+stul_qw2, 'шт, стоимость', stul_cost, 'руб')
 
 # Вывести стоимость каждого товара на складе: один раз распечать сколько всего столов, стульев и т.д. на складе
 # Формат строки <товар> - <кол-во> шт, стоимость <общая стоимость> руб
