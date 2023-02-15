1eb1dd2bd00eee99321c48c5db5b113cb142ba55
Админ <sm_s@tut.by>
2023-02-15 16:18:56 +0300

    int

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -10,17 +10,30 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+
+def inconv(paper_x, paper_y):
+    if paper_x < envelop_x and paper_y < envelop_y:
+        print(paper_x, paper_y, 'В конверте')
+    else: print(paper_x, paper_y, 'Не помещается')
+
+
 # проверить для
-# paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
-# paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
+
+paper_x, paper_y = 9, 8
+inconv(paper_x, paper_y)
+paper_x, paper_y = 6, 8
+inconv(paper_x, paper_y)
+paper_x, paper_y = 8, 6
+inconv(paper_x, paper_y)
+paper_x, paper_y = 3, 4
+inconv(paper_x, paper_y)
+paper_x, paper_y = 11, 9
+inconv(paper_x, paper_y)
+paper_x, paper_y = 9, 11
+inconv(paper_x, paper_y)
+
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,25 +41,54 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+hole_x, hole_y = 8, 9
+def incert(x, y, z):
+    if hole_x > x and hole_y > y:
+        print('Пройдет по xy')
+    elif hole_x > y and hole_y > z:
+        print('Пройдет по yz')
+    elif hole_x > x and hole_y > z:
+        print('Пройдет по zx')
+    else:
+        print ('Не пройдет')
+
+
+brick_x, brick_y, brick_z = 11, 10, 2
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 11, 2, 10
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 10, 11, 2
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 10, 2, 11
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 2, 10, 11
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 2, 11, 10
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 3, 5, 6
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 3, 6, 5
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 6, 3, 5
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 6, 5, 3
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 5, 6, 3
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 5, 3, 6
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 11, 3, 6
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 11, 6, 3
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 6, 11, 3
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 6, 3, 11
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 3, 6, 11
+incert(brick_x, brick_y, brick_z)
+brick_x, brick_y, brick_z = 3, 11, 6
+incert(brick_x, brick_y, brick_z)
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
