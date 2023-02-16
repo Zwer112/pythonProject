65dc49295ddc1a7764cbf26c1c5f45b8bb9dbdf2
Админ <sm_s@tut.by>
2023-02-16 16:08:15 +0300

    new rot

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -10,11 +10,14 @@ sd.resolution = (800, 600)
 # Вывести 10 смайликов в произвольных точках экрана.
 def smile():
     point = sd.random_point()
-    point_l = sd.get_point(point.x - 15, point.y + 10)
-    point_r = sd.get_point(point.x + 15, point.y + 10)
-    sd.circle(center_position=point, radius=50, width=5)
-    sd.circle(center_position=point_l, radius=5, width=1)
-    sd.circle(center_position=point_r, radius=5, width=1)
+    point_l = sd.get_point(point.x - 20, point.y + 10)
+    point_r = sd.get_point(point.x + 20, point.y + 10)
+    point_ll = sd.get_point(point.x - 30, point.y - 30)
+    point_rr = sd.get_point(point.x + 30, point.y - 20)
+    sd.circle(center_position=point, radius=50, width=0)
+    sd.circle(center_position=point_l, radius=15, color=(0, 0, 0), width=0)
+    sd.circle(center_position=point_r, radius=15, color=(0, 0, 0), width=0)
+    sd.rectangle(left_bottom=point_ll, right_top=point_rr, color=(0, 0, 0), width=0)
 
 
 for _ in range(10):
