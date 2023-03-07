2d7319663200be887e53350e337e7058318573c3
Админ <sm_s@tut.by>
2023-03-07 15:03:44 +0300

    new rot

---
+++ lesson_004/04_fractal.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import simple_draw as sd
+sd.resolution = (1200, 600)
+
+# 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
+# Функция должна принимать параметры:
+# - точка начала рисования,
+# - угол рисования,
+# - длина ветвей,
+# Отклонение ветвей от угла рисования принять 30 градусов,
+
+# 2) Сделать draw_branches рекурсивной
+# - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
+# - вызывать саму себя 2 раза из точек-концов нарисованных ветвей,
+#   с параметром "угол рисования" равным углу только что нарисованной ветви,
+#   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
+
+# 3) первоначальный вызов:
+# root_point = get_point(300, 30)
+# draw_bunches(start_point=root_point, angle=90, length=100)
+
+# Пригодятся функции
+# sd.get_point()
+# sd.get_vector()
+# Возможный результат решения см lesson_004/results/exercise_04_fractal_01.jpg
+
+# можно поиграть -шрифтами- цветами и углами отклонения
+
+# TODO здесь ваш код
+root_point = sd.get_point(300, 0)
+def draw_bunches(start_point=root_point, angle=90, length=100):
+    v1 = sd.get_vector(start_point, angle, length, width=3)
+    v1.draw()
+    if length < 4:
+        return
+    next_point = v1.end_point
+    next_length = length * (sd.random_number(60, 90))/100
+    next_angle =  angle - sd.random_number(28, 42)
+    draw_bunches(start_point=next_point, angle=next_angle, length=next_length)
+    next_angle = angle + sd.random_number(28, 42)
+    draw_bunches(start_point=next_point, angle=next_angle, length=next_length)
+
+
+
+draw_bunches(root_point, 90, 100)
+
+# 4) Усложненное задание (делать по желанию)
+# - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
+# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
+# Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
+
+# Пригодятся функции
+# sd.random_number()
+
+sd.pause()
+
+
